b8b87c8ac05ef519bf5d6c7ca75dfaa2d3e341f8
josefuccef <josefuccef@gmail.com>
2025-02-15 00:53:03 +0100

    first commit v1

--- index.js
+++ index.js
@@ -13,24 +13,26 @@ app.use(methodOverride("_method"));
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const path = require('path');
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// إعداد مجلد الملفات الثابتة
+app.use(express.static(path.join(__dirname, 'public')));
 
 // الوصول إلى رابط قاعدة البيانات من متغيرات البيئة
 const dbURL = "mongodb+srv://josefmegane:mR47S2iaRxxR4ELS@cluster0.jmivy.mongodb.net/alldata?retryWrites=true&w=majority&appName=Armedis";
 
 const store = new MongoDBStore({
-    uri: dbURL,
-    collection: "sessions"
+ uri: dbURL,
+ collection: "sessions"
 });
 
 app.use(
-    session({
-        secret: "mySuperSecretKeyhellobrder166628", // مفتاح تشفير الجلسات
-        resave: false, // لا تعيد حفظ الجلسة إذا لم يتم تعديلها
-        saveUninitialized: false, // لا تحفظ الجلسات الفارغة
-        store: store // ربط الجلسة بمخزن MongoDB
-    })
+ session({
+  secret: "mySuperSecretKeyhellobrder166628", // مفتاح تشفير الجلسات
+  resave: false, // لا تعيد حفظ الجلسة إذا لم يتم تعديلها
+  saveUninitialized: false, // لا تحفظ الجلسات الفارغة
+  store: store // ربط الجلسة بمخزن MongoDB
+ })
 );
 
 // استخدام express-flash
@@ -50,11 +52,11 @@ app.use("/", login);
 
 // connect to server mongodb par mongoose
 mongoose
-    .connect(dbURL).then(() => {
-        app.listen(port, (req, res) => {
-            console.log(`localhost:${port}`, "connect is seccese");
-        });
-    })
-    .catch(ererr => {
-        console.log(ererr, "your problme conx");
-    });
+ .connect(dbURL).then(() => {
+  app.listen(port, (req, res) => {
+   console.log(`localhost:${port}`, "connect is seccese");
+  });
+ })
+ .catch(ererr => {
+  console.log(ererr, "your problme conx");
+ });
